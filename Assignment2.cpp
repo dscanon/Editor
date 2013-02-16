2a6edcf5f1a7dbba15778d52ea731bd6b806352e
dscanon <dschanoncanon@gmail.com>
2013-02-16 13:49:22 +0700

    add if condition

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -5,6 +5,8 @@
 #include<map>
 using namespace std;
 void readFile(ifstream infile,string data);
+void setDateInFile(string data);
+void setDateOnShow(string data);
 int main(){
 	string str,command, data;
 getline(cin,str);
@@ -16,16 +18,17 @@ ifstream infile;
 	data	= str.substr(str.find(' ')+1);
 	cout << command << endl;
 	cout << data << endl;
-	switch(command)
-	{
-	case "#Load": readFile(infile,data);break;
-	case "#Set"	: break;
-	case "#Show": break;
-	case "#Edit": break;
-	case "#Save": break;
-	case "#Quit": break;
-	default : cout <<"Incorrect command"; break;
-	}
+	if(command == "#Load"){readFile(infile,data);} 
+	//readFile will load file name(data) and get data in <map>.
+	else if(command == "#Set"){setDateInFile(data);setDateOnShow(data);}
+	/*setDateInFile is method will set date same as date in file.
+	 ex.  #Set12 01 2013	has set		string dateInFile 12012013
+	 setDateOnShow is method will set date on show content.
+	 ex. #Set12 01 2013	has set		string dateOnshow 12 December 2013*/	 
+	else if(command == "#Show"){}
+	else if(command == "#Edit"){}
+	else if(command == "#Save"){}
+	else if(command == "#Quit")
 return 0;
 }
 
