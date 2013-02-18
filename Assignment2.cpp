2c12987c70bf05cfe6e631e1d126ba1118c787da
dscanon <dschanoncanon@gmail.com>
2013-02-19 01:55:14 +0700

    add  #Set #Show
    #Load #Set #Show complete.Next time add #Edit #save #Quit.
    19/02/2556 1:55am

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -5,17 +5,16 @@
 #include<map>
 using namespace std;
 
-
 void readfile(ifstream & infile, map<string, string> & smap);
-
+bool showContent(map<string, string> & smap, string date);
 
 int main(){
 	 /* For example #Load wiboon.txt
 	 *  command ex. #Load #Set #Save
 	 *  data is argument after command
 	 */
-	string str, command, data, date;
-	
+	string str, command, data, date, month ;
+
 	ifstream infile;
 	
 	//key = date, value = messages which are matched with date.
@@ -25,8 +24,6 @@ int main(){
 	while(getline(cin, str)){
 	data = str.substr(str.find(' ')+1);
 	command = str.substr(0,str.find(' '));
-	cout << command << endl;
-	cout << data << endl;
 		if(smap.empty()){
 			if(command == "#Load"){
 				smap.clear();
@@ -47,9 +44,32 @@ int main(){
 				else readfile(infile, smap);
 			}
 			else if(command == "#Set"){
-				date == data;
+				date = data;
+				month = date.substr(date.find(' ')+1,date.rfind(' ')-date.find(' ')-1);
+				cout << month  << endl;
+					if(month=="January")		{month = "01";}
+					else if(month=="February")	{month = "02";}
+					else if(month=="March")		{month = "03";}
+					else if(month=="April")		{month = "04";}
+					else if(month=="May")		{month = "05";}
+					else if(month=="June")		{month = "06";}
+					else if(month=="July")		{month = "07";}
+					else if(month=="August")	{month = "08";}
+					else if(month=="September")	{month = "09";}
+					else if(month=="October")	{month = "10";}
+					else if(month=="November")	{month = "11";}
+					else if(month=="December")	{month = "12";}
+						cout << month << endl;
+						date = date.replace(date.find(' '),date.rfind(' ')-date.find(' ')+1,month);
+						cout << date <<endl;
 			}
 			else if(command == "#Show"){
+				if(showContent(smap,date)){
+					cout << smap[date] << endl;
+				}
+				else {
+					cout << "No date in file.";
+				}
 			}
 			else if(command == "#Edit"){
 			}
@@ -70,4 +90,14 @@ void readfile(ifstream &infile, map<string, string> & smap){
 		smap[str.substr(0,8)] = str.substr(9,91); 
 		cout << smap[str.substr(0,8)] << endl;
 	}
+}
+
+
+bool showContent(map<string, string> & smap, string date){
+	for(map<string, string>::iterator it=smap.begin();it!=smap.end();it++){
+		if((*it).first == date ){
+			return 1;
+		}
+	}
+return 0;
 }
